a17c01609057d0f73e1148b6b48ef8fbc15c8a4e
Rajiv Gupta <rajiv.gupta0@walmart.com>
2019-07-27 15:52:26 +0530

    Binary Tree Traversals

---
+++ src/BinaryTree/BinaryTreeTraversalUtil.java
@@ -0,0 +1,238 @@
+package BinaryTree;
+
+import sun.jvm.hotspot.memory.Space;
+
+import java.util.*;
+
+public class BinaryTreeTraversalUtil {
+
+    public static void PreOrderTraversal (TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        PreOrderTraversal(root.left);
+        PreOrderTraversal(root.right);
+    }
+
+    public static void InOrderTraversal (TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        InOrderTraversal(root.left);
+        System.out.print(root.data + " ");
+        InOrderTraversal(root.right);
+    }
+
+    public static void PostOrderTraversal (TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        PostOrderTraversal(root.left);
+        PostOrderTraversal(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void LevelOrderTraversal (TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        List<TreeNode> queue = new ArrayList<>();
+        queue.add(root);
+        while (queue.size() > 0) {
+            int count = queue.size();
+            while (count > 0) {
+                TreeNode node = queue.remove(0);
+                System.out.print(node.data + " ");
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                count--;
+            }
+        }
+    }
+
+    public static void LeftView (TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        List<TreeNode> queue = new ArrayList<>();
+        queue.add(root);
+        while (queue.size() > 0) {
+            int count = queue.size();
+            int queueCount = queue.size();
+            while (count > 0) {
+                TreeNode node = queue.remove(0);
+                if (count == queueCount) {
+                    System.out.print(node.data + " ");
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                count--;
+            }
+        }
+    }
+
+    public static void RightView (TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        List<TreeNode> queue = new ArrayList<>();
+        queue.add(root);
+        while (queue.size() > 0) {
+            int count = queue.size();
+            while (count > 0) {
+                TreeNode node = queue.remove(0);
+                if (count == 1) {
+                    System.out.print(node.data + " ");
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                count--;
+            }
+        }
+    }
+
+    public static void SpiralLevelOrderTraversal (TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        stack1.push(root);
+        while (stack1.size() > 0 || stack2.size() > 0) {
+            while (stack1.size() > 0) {
+                TreeNode node = stack1.pop();
+                System.out.print(node.data + " ");
+                if (node.left != null) {
+                    stack2.push(node.left);
+                }
+                if (node.right != null) {
+                    stack2.push(node.right);
+                }
+            }
+            System.out.println();
+            while (stack2.size() > 0) {
+                TreeNode node = stack2.pop();
+                System.out.print(node.data + " ");
+                if (node.right != null) {
+                    stack1.push(node.right);
+                }
+                if (node.left != null) {
+                    stack1.push(node.left);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static class VOT_Obj {
+        public TreeNode node;
+        public int dis;
+
+        VOT_Obj(TreeNode node, int dis) {
+            this.node = node;
+            this.dis = dis;
+        }
+    }
+
+    public static void VerticalOrderTraversal_SpaceComplexityN (TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Map<Integer, List<TreeNode>> distMap = new TreeMap<>();
+        List<VOT_Obj> queue = new ArrayList<>();
+        queue.add(new VOT_Obj(root, 0));
+
+        while (queue.size() > 0) {
+            VOT_Obj obj = queue.remove(0);
+            if (distMap.containsKey(obj.dis)) {
+                distMap.get(obj.dis).add(obj.node);
+            } else {
+                List<TreeNode> nodeObj = new ArrayList<>();
+                nodeObj.add(obj.node);
+                distMap.put(obj.dis, nodeObj);
+            }
+
+            if (obj.node.left != null) {
+                queue.add(new VOT_Obj(obj.node.left, obj.dis - 1));
+            }
+            if (obj.node.right != null) {
+                queue.add(new VOT_Obj(obj.node.right, obj.dis + 1));
+            }
+        }
+        for (Integer key : distMap.keySet()) {
+            List<TreeNode> nodeList = distMap.get(key);
+            for (TreeNode node : nodeList) {
+                System.out.print(node.data + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void TopView (TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Map<Integer, TreeNode> distMap = new TreeMap<>();
+        List<VOT_Obj> queue = new ArrayList<>();
+        queue.add(new VOT_Obj(root, 0));
+        while (queue.size() > 0) {
+            VOT_Obj obj = queue.remove(0);
+            if (!distMap.containsKey(obj.dis)) {
+                distMap.put(obj.dis, obj.node);
+            }
+
+            if (obj.node.left != null) {
+                queue.add(new VOT_Obj(obj.node.left, obj.dis-1));
+            }
+            if (obj.node.right != null) {
+                queue.add(new VOT_Obj(obj.node.right, obj.dis+1));
+            }
+        }
+        for (Integer key : distMap.keySet()) {
+            System.out.print(distMap.get(key).data + " ");
+        }
+    }
+
+    public static void BottomView (TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Map<Integer, TreeNode> distMap = new TreeMap<>();
+        List<VOT_Obj> queue = new ArrayList<>();
+        queue.add(new VOT_Obj(root, 0));
+        while (queue.size() > 0) {
+            VOT_Obj obj = queue.remove(0);
+            distMap.put(obj.dis, obj.node);
+
+            if (obj.node.left != null) {
+                queue.add(new VOT_Obj(obj.node.left, obj.dis-1));
+            }
+            if (obj.node.right != null) {
+                queue.add(new VOT_Obj(obj.node.right, obj.dis+1));
+            }
+        }
+        for (Integer key : distMap.keySet()) {
+            System.out.print(distMap.get(key).data + " ");
+        }
+    }
+
+    public static int HeightOfBinaryTree (TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(HeightOfBinaryTree(root.left), HeightOfBinaryTree(root.right)) + 1;
+    }
+}
---
+++ src/BinaryTree/MainProgram.java
@@ -0,0 +1,92 @@
+package BinaryTree;
+
+public class MainProgram {
+    public static void main (String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.println("\n\nBinary Tree: Pre Order Traversal");
+        BinaryTreeTraversalUtil.PreOrderTraversal(root);
+
+        System.out.println("\n\nBinary Tree: In Order Traversal");
+        BinaryTreeTraversalUtil.InOrderTraversal(root);
+
+        System.out.println("\n\nBinary Tree: Post Order Traversal");
+        BinaryTreeTraversalUtil.PostOrderTraversal(root);
+
+//        TreeNode root1 = new TreeNode(1);
+//        root1.left = new TreeNode(2);
+//        root1.right = new TreeNode(3);
+//        root1.left.left = new TreeNode(4);
+//        root1.left.right = new TreeNode(5);
+//        root1.left.left.left = new TreeNode(6);
+//        root1.left.right.right = new TreeNode(7);
+//        root1.right.right = new TreeNode(8);
+//        root1.right.right.right = new TreeNode(9);
+
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(2);
+        root1.right = new TreeNode(3);
+        root1.left.left = new TreeNode(4);
+        root1.left.right = new TreeNode(5);
+        root1.right.left = new TreeNode(6);
+        root1.right.right = new TreeNode(7);
+        root1.right.left.right = new TreeNode(8);
+
+        System.out.println("\n\nBinary Tree: Level Order Traversal");
+        BinaryTreeTraversalUtil.LevelOrderTraversal(root1);
+
+        System.out.println("\n\nBinary Tree: Left View");
+        BinaryTreeTraversalUtil.LeftView(root1);
+
+        System.out.println("\n\nBinary Tree: Right View");
+        BinaryTreeTraversalUtil.RightView(root1);
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(3);
+        root2.left.left = new TreeNode(4);
+        root2.left.right = new TreeNode(5);
+        root2.left.right.left = new TreeNode(17);
+        root2.left.right.left.left = new TreeNode(18);
+        root2.left.right.right = new TreeNode(6);
+        root2.right.left = new TreeNode(18);
+        root2.right.right = new TreeNode(7);
+        root2.right.right.right = new TreeNode(8);
+
+        System.out.println("\n\nBinary Tree: Spiral Level Order Traversal");
+        BinaryTreeTraversalUtil.SpiralLevelOrderTraversal(root2);
+
+        TreeNode root3 = new TreeNode(1);
+        root3.left = new TreeNode(2);
+        root3.right = new TreeNode(3);
+        root3.left.left = new TreeNode(4);
+        root3.left.right = new TreeNode(5);
+        root3.right.left = new TreeNode(6);
+        root3.right.right = new TreeNode(7);
+        root3.right.left.right = new TreeNode(8);
+
+        System.out.println("\n\nBinary Tree: Vertical Order Traversal");
+        BinaryTreeTraversalUtil.VerticalOrderTraversal_SpaceComplexityN(root3);
+
+        System.out.println("\nBinary Tree: Top View");
+        BinaryTreeTraversalUtil.TopView(root3);
+
+        System.out.println("\n\nBinary Tree: Bottom View");
+        BinaryTreeTraversalUtil.BottomView(root3);
+
+        TreeNode root4 = new TreeNode(1);
+        root4.left = new TreeNode(2);
+        root4.right = new TreeNode(6);
+        root4.left.left = new TreeNode(3);
+        root4.left.right = new TreeNode(4);
+        root4.left.right.right = new TreeNode(5);
+        root4.left.right.right.right = new TreeNode(7);
+        root4.left.right.right.right.right = new TreeNode(8);
+
+        System.out.println("\n\nHeight of Binary Tree: " + BinaryTreeTraversalUtil.HeightOfBinaryTree(root4));
+    }
+}
